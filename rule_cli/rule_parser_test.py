b37c9d25053f7f31cc480a2fe62c0caf1af523d4
Fred Xia <fxia@proofpoint.com>
2020-05-19 13:19:53 -0700

    update

---
+++ rule_cli/rule_parser_test.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+
+import os, sys
+import pdb
+
+script_dir = os.path.dirname(__file__)
+if not script_dir.startswith("/"):
+    script_dir = os.getcwd() + "/" + script_dir
+sys.path.append(script_dir)
+
+from rule_parser import Keyword, Value, Context, IntValue, IntRange, RunTime
+
+class MyContext(Context):
+    def __init__(self):
+        Context.__init__(self, "my_context")
+
+my_context = MyContext()
+
+cmd = Keyword("my_cmd")
+cmd.next_arg_is("cmd1", IntValue("my_int_value"))
+cmd.next_arg_is("cmd2").next_arg_is("cmd3", optional=True)
+
+rangeValue = IntRange("range1", 10, 20, help_string="Range1 help")
+value_node = cmd.get_path("my_cmd/cmd1/my_int_value")
+value_node.next_arg_is("end_cmd")
+
+value_node.next_arg_is("cmd3", help="Cmd2 help", value=rangeValue, optional=True).\
+    next_is("cmd4", help="End at cmd4")
+
+value_node.next_args_are([("cmd4", "Cmd4 help"),
+                          ("cmd5", "Cmd5 help", rangeValue)])
+
+value_node.next_arg_is("cmd5", IntValue("value2"), optional=True).next_is("cmd4")
+
+def my_command_handler(cmd_context, tokens, values):
+    print("%s calling my_command_handler, tokens %s, values %s" % (
+        cmd_context.name, tokens, values))
+
+my_context.register_command(cmd, discard_garbage_tokens=False,
+                            handler=my_command_handler)
+
+cmd = my_context.get_command("my_cmd")
+cmd.print_commands()
+
+run_time = RunTime("runtime", my_context)
+run_time.evaluate_command("my_cmd cmd1 2")
+run_time.evaluate_command("my_cmd cmd2 cmd3")
+run_time.evaluate_command("my_cmd cmd2")
+run_time.evaluate_command("my_cmd cmd1 2 end_cmd")
+run_time.evaluate_command("my_cmd cmd1 2 cmd3 10 cmd4")
+run_time.evaluate_command("my_cmd cmd1 2 cmd4")
